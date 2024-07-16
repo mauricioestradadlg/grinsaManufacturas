1c0583668c637e1cdfec274c947a0a80d18fd954
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-16 13:45:09 -0600

    numero whatsapp grinsa

--- frontend/src/componentes/WhatsApp.js
+++ frontend/src/componentes/WhatsApp.js
@@ -4,6 +4,6 @@ export default function WhatsApp(){
 
     return(
     <>
-        <a href="https://wa.me/8115887060" target="_blank" class="whatsapp-button"><img alt = "" src = '../imagenes/botonWha.png' height="150" width="150" class="whatsapp-icon"/></a>
+        <a href="https://wa.me/8134136152" target="_blank" class="whatsapp-button"><img alt = "" src = '../imagenes/botonWha.png' height="150" width="150" class="whatsapp-icon"/></a>
     </>);
 }
