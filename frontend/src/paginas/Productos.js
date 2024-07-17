2ab939fc1f7fb185c593b91a47acd24265419505
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-16 20:54:22 -0600

    avances de la pagina

--- frontend/src/paginas/Productos.js
+++ frontend/src/paginas/Productos.js
@@ -10,92 +10,96 @@ export default function Producto(){
             <h2>PAILERIA</h2><br/><br/>
 
             <h3>Fabricación y montaje de estructura</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p1.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p2.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p3.png" alt="producto"/>
             <br/>
 
             <h3>Mezzanines</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p4.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p5.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p6.png" alt="producto"/>
             <br/>
 
             <h3>Anclas para construcción</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p7.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p8.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p9.png" alt="producto"/>
             <br/>
 
             <h3>Process Skids</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p10.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p11.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p12.png" alt="producto"/>
             <br/>
 
             <h3>Ducteria</h3>
             <p>ductos, transiciones, reducciones, bayonetas, gorros
             chinos tees, insertos, codos.
             </p><br/>
-            <img id ="imgProducto"src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p13.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p14.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p15.png" alt="producto"/>
             <br/>
 
             <h3>Hornos</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p16.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p17.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p18.png" alt="producto"/>
             <br/>
 
             <h3>Estructura para celdas automotrices</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p19.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p20.png" alt="producto"/>
             <br/>
 
             <h3>Tanques</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p21.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p22.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p23.png" alt="producto"/>
             <br/>
 
             <h3>Racks</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p24.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p25.png" alt="producto"/>
             <br/>
 
             <h3>Tuberia de proceso</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p26.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p27.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p28.png" alt="producto"/>
             <br/>
 
             <h3>Tolvas y silos</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p29.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p30.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p31.png" alt="producto"/>
             <br/>
 
             <h3>Gabinetes de control con certificación UL50</h3><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p32.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p33.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p34.png" alt="producto"/>
             <br/>
 
             <h3>Maquila y fabricación</h3><br/><br/>
-            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
-            <br/>
+            <img id ="imgProducto" src="./imagenes/p35.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p36.png" alt="producto"/>
+            <img id ="imgProducto" src="./imagenes/p37.png" alt="producto"/>
+            <br/><br/><br/>
 
 
 
             <h2>MAQUINADOS</h2><br/><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p><br/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Sed mattis dui sed aliquam porta. Nam porta nulla at felis sagittis, 
-                quis vehicula sapien tempor. Suspendisse vitae enim a mi bibendum rutrum. 
-                Phasellus neque magna, vulputate vel tortor sed, tempor rutrum orci. 
-                Vivamus eget lorem justo. Quisque tristique consequat dolor eget consectetur.
-            </p><br/>
-           
+
+            <h3>Centro de maquinado CNC, fresadora y rectificado plano</h3><br/><br/>
+            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <br/>
+
+            <h3>Torno CNC, convencional y rectificado cilíndrico</h3><br/><br/>
+            <img id ="imgProducto" src="" alt="producto"/><img src="" alt="producto"/><img src="" alt="producto"/>
+            <br/>
+    
         </main>
     );
 }
