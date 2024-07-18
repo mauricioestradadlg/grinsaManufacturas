34d74d5f1e1461ab4b5cd2bb94b4ca0833d8795f
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-18 12:07:07 -0600

    formEnviado modificado

--- frontend/src/paginas/FormEnviado.js
+++ frontend/src/paginas/FormEnviado.js
@@ -9,22 +9,30 @@ function FormEnviado() {
     return (
       <>
         <main>
-          <h1 style={textStyle} id="h1AU">Muchas gracias por contactarnos</h1>
-          <h3 style={textStyle} id="h1AU">Te contactaremos pronto!</h3>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
-          <p  style={textStyle} className="parrafoAU">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magni commodi neque eligendi exercitationem molestias rerum accusamus laudantium vero nostrum dicta minima suscipit nam obcaecati animi pariatur, doloremque ex expedita.</p>
+          <h1 style={textStyle} id="h1AU">Muchas gracias por contactarnos</h1><br/>
+          <h3 style={textStyle} classname ="AU"id="h1AU">Nos comunicaremos pronto contigo!</h3><br/><br/>
+
+          
+
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+        
+         
+          
         </main>
       </>
     );
